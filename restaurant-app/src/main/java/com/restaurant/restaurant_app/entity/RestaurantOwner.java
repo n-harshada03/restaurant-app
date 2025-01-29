bfb796a355ccac191edeb9c0ffd0411a0b08a329
n-harshada03 <harshdanavale2003@gmail.com>
2025-01-29 23:00:10 +0530

    restaurant-owner entity details

---
+++ restaurant-app/src/main/java/com/restaurant/restaurant_app/entity/RestaurantOwner.java
@@ -0,0 +1,50 @@
+package com.restaurant.restaurant_app.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name="restaurant_owner")
+@Getter
+@Setter
+public class RestaurantOwner {
+
+    @Id
+    @Column(name = "owner_id")
+    private int ownerId;
+
+    @Column(name="first_name",length=100, nullable = false)
+    private String firstName;
+
+    @Column(name="middle_name",length=100)
+    private String middleName;
+
+    @Column(name="last_name",length=100, nullable = false)
+    private String lastName;
+
+    @Column(name="country_code",length=10, nullable = false)
+    private String countryCode;
+
+    @Column(name="mobile",length=10, nullable = false,unique = true)
+    private String mobile;
+
+    @Column(name="email",length=100, nullable = false,unique = true)
+    private String email;
+
+
+    @Enumerated(EnumType.STRING)
+    @Column(name="government_id_type", nullable = false)
+    private GovernmentIdType governmentIdType;
+
+    @Column(name="government_id",length=20, nullable = false,unique = true)
+    private String government_id;
+
+    @Column(name="password",length = 100, nullable = false)
+    private String password;
+
+    public enum GovernmentIdType{
+        Pancard, Aadharcard;
+    }
+
+}
