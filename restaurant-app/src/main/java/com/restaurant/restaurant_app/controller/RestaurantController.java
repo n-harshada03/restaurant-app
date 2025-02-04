b63357726688d9eeb1b76a75f6ccd5d79b106b18
n-harshada03 <harshdanavale2003@gmail.com>
2025-02-04 16:31:52 +0530

    controller class for handling requests/responses

---
+++ restaurant-app/src/main/java/com/restaurant/restaurant_app/controller/RestaurantController.java
@@ -0,0 +1,30 @@
+package com.restaurant.restaurant_app.controller;
+
+import com.restaurant.restaurant_app.models.RestaurantRequest;
+import com.restaurant.restaurant_app.service.RestaurantService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/restro")
+public class RestaurantController {
+
+    private final RestaurantService restaurantService;
+
+    public RestaurantController(RestaurantService restaurantService) { //dependency injected
+        this.restaurantService = restaurantService;
+    }
+
+    @GetMapping("/new")
+    public ResponseEntity<String> helloWorld(){
+        return new ResponseEntity<>("Hello from API", HttpStatus.OK);
+    }
+
+    @PostMapping("/create")
+    public ResponseEntity<String> registerRestaurant(@RequestBody RestaurantRequest restaurantRequest){
+        restaurantService.addRestaurant(restaurantRequest);
+        return new ResponseEntity<>("Restro name is " + restaurantRequest.getRestroName() + " and restro type is " + restaurantRequest.getRestroType(),HttpStatus.OK);
+    }
+
+}
