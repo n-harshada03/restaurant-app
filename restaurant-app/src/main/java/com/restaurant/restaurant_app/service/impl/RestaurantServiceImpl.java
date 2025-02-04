f63410eaa4327a6f4d571845245f2ad4012c4cd3
n-harshada03 <harshdanavale2003@gmail.com>
2025-02-04 16:14:41 +0530

    converting restaurant request DTO into entity(restaurant)

--- restaurant-app/src/main/java/com/restaurant/restaurant_app/service/impl/RestaurantServiceImpl.java
+++ restaurant-app/src/main/java/com/restaurant/restaurant_app/service/impl/RestaurantServiceImpl.java
@@ -1,14 +1,42 @@
 package com.restaurant.restaurant_app.service.impl;
 
+import com.restaurant.restaurant_app.entity.Restaurant;
 import com.restaurant.restaurant_app.models.RestaurantRequest;
+import com.restaurant.restaurant_app.repository.RestaurantRepository;
 import com.restaurant.restaurant_app.service.RestaurantService;
 import org.springframework.stereotype.Service;
+import org.springframework.util.ObjectUtils;
 
 @Service
 public class RestaurantServiceImpl implements RestaurantService {
 
+    private final RestaurantRepository restaurantRepository;
+
+    public RestaurantServiceImpl(RestaurantRepository restaurantRepository) { //dependency injected
+        this.restaurantRepository = restaurantRepository;
+    }
+
     @Override
     public boolean addRestaurant(RestaurantRequest restaurantRequest) {
-        return false;
+        //Converts the RestaurantRequest DTO into a Restaurant entity
+        Restaurant restaurant=mapDTOtoEntity(restaurantRequest);
+        Restaurant persistedDetails= restaurantRepository.save(restaurant); //save to database
+
+        if (ObjectUtils.isEmpty(persistedDetails)) {
+            return false;
+        }
+
+        return true;
+    }
+
+    private Restaurant mapDTOtoEntity(RestaurantRequest restaurantRequest) {
+        return Restaurant.builder()
+                .restroName(restaurantRequest.getRestroName())
+                .restroType(restaurantRequest.getRestroType())
+                .serviceType(restaurantRequest.getServiceType())
+                .speciality(restaurantRequest.getSpeciality())
+                .openingHour(restaurantRequest.getOpening_hour())
+                .closingHour(restaurantRequest.getClosing_hour())
+                .build();
     }
 }
